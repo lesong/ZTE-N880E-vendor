3fe7c35e8df668a0b29843dbc646220803651d67
lesong <137701932@qq.com>
2013-04-23 18:44:20 +0800

    Test

---
+++ camera/camera-vendor-blobs1.mk
@@ -0,0 +1,27 @@
+
+PRODUCT_COPY_FILES += \
+    vendor/zte/n880e/camera/lib/libgemini.so:system/lib/libgemini.so \
+    vendor/zte/n880e/camera/lib/libmmipl.so:system/lib/libmmipl.so \
+    vendor/zte/n880e/camera/lib/libmmjpeg.so:system/lib/libmmjpeg.so \
+    vendor/zte/n880e/camera/lib/libchromatix_imx074_default_video.so:system/lib/libchromatix_imx074_default_video.so \
+    vendor/zte/n880e/camera/lib/libchromatix_imx074_preview.so:system/lib/libchromatix_imx074_preview.so \
+    vendor/zte/n880e/camera/lib/libchromatix_mt9p017_ar.so:system/lib/libchromatix_mt9p017_ar.so \
+    vendor/zte/n880e/camera/lib/libchromatix_mt9p017_default_video.so:system/lib/libchromatix_mt9p017_default_video.so \
+    vendor/zte/n880e/camera/lib/libchromatix_mt9p017_preview.so:system/lib/libchromatix_mt9p017_preview.so \
+    vendor/zte/n880e/camera/lib/libchromatix_ov2720_default_video.so:system/lib/libchromatix_ov2720_default_video.so \
+    vendor/zte/n880e/camera/lib/libchromatix_ov2720_preview.so:system/lib/libchromatix_ov2720_preview.so \
+    vendor/zte/n880e/camera/lib/libchromatix_s5k4e1_ar.so:system/lib/libchromatix_s5k4e1_ar.so \
+    vendor/zte/n880e/camera/lib/libchromatix_s5k4e1_default_video.so:system/lib/libchromatix_s5k4e1_default_video.so \
+    vendor/zte/n880e/camera/lib/libchromatix_s5k4e1_preview.so:system/lib/libchromatix_s5k4e1_preview.so \
+    vendor/zte/n880e/camera/lib/hw/camera.msm7627a.so:system/lib/hw/camera.msm7627a.so \
+    vendor/zte/n880e/camera/lib/liboemcamera.so:system/lib/liboemcamera.so \
+    vendor/zte/n880e/camera/lib/libcontrast.so:system/lib/libcontrast.so \
+    vendor/zte/n880e/camera/lib/libcamera_client.so:system/lib/libcamera_client.so
+    vendor/zte/n880e/camera/lib/libcameraservice.so:system/lib/libcameraservice.so
+    
+PRODUCT_COPY_FILES += \
+    vendor/zte/n880e/camera/bin/mm-qcamera-daemon:system/bin/mm-qcamera-daemon \
+    vendor/zte/n880e/camera/bin/mm-qcamera-test:system/bin/mm-qcamera-test \
+    vendor/zte/n880e/camera/bin/mm-qcamera-testsuite-client:system/bin/mm-qcamera-testsuite-client \
+    vendor/zte/n880e/camera/bin/test_gemini:system/bin/test_gemini \
+    vendor/zte/n880e/camera/bin/v4l2-qcamera-app:system/bin/v4l2-qcamera-app
